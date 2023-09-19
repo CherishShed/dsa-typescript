b6abc12e72cb6996dc86a4142a2e7a97b28a7b64
CherishShed <c.shed2000@gmail.com>
2023-09-19 18:56:22 +0100

    norm fib

--- src/normalFibonacci.ts
+++ src/normalFibonacci.ts
@@ -1,9 +1,9 @@
-function fibbonaci(num: number) {
+function fibbonaci(num: number): number[] {
   const fib: number[] = [0, 1];
   for (let i = 2; i < num; i++) {
     fib.push(fib[i - 1] + fib[i - 2]);
   }
-  console.log(fib);
+  return fib;
 }
 
-fibbonaci(8);
+console.log(fibbonaci(8));
