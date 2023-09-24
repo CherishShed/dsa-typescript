e636595116740970f1df087848528d9e6250a1ed
CherishShed <c.shed2000@gmail.com>
2023-09-24 14:59:38 +0100

    bubble sort

---
+++ src/bubbleSort.ts
@@ -0,0 +1,12 @@
+function bubbleSort(arr: number[]) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([1, 3, 5, 2, 6]));
